9bfb0437d3bf785a707a0d2bdebfd2f27a0a94d6
Aluno <Aluno@Digital.net>
2018-06-07 15:55:02 -0300

    Apagou tela login, Alterção na Tela Acesso e acesso com o banco

--- mil_grau/TelaAcesso.aspx.cs
+++ mil_grau/TelaAcesso.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace mil_grau
 {
@@ -13,5 +15,48 @@ namespace mil_grau
         {
         }
 
+        protected void btnLogar_Click(object sender, EventArgs e)
+        {
+            string codigo = txtCod.Text;
+
+            string Nome = null, codArduino = null;
+
+            if (codigo == "")
+            {
+                lblErro.Text = "Preencha um Código";
+            }
+            else
+            {
+
+                using (SqlConnection conn = new SqlConnection("Server=tcp:milgrau.database.windows.net,1433;Initial Catalog=cardapio;Persist Security Info=False;User ID=picanha;Password=info@211;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30"))
+                {
+                    conn.Open();
+
+                    // Cria um comando para selecionar registros da tabela, trazendo todas as pessoas que nasceram depois de 1/1/1900
+                    using (SqlCommand cmd = new SqlCommand("select * from Acesso where Cod_arduino = "+codigo, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            //Obtém os registros, um por vez
+                            while (reader.Read() == true)
+                            {
+
+                                Nome = reader["Nome"].ToString();
+                                codArduino = reader["Cod_arduino"].ToString();
+                            }
+                        }
+                    }
+                }
+
+                if(Nome == "" && codArduino == "")
+                {
+                    lblErro.Text = "Código não localizado";
+                } else
+                {
+                    Response.Redirect("grelha.aspx");
+                }
+
+            }
+        }
     }
 }
